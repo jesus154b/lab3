db135db2da3001e174dcb4d24481b2c1d2071299
Jesus Oviedo <jaoviedo@cs.ucsb.edu>
2023-02-15 17:33:46 -0800

    p1 changes

--- ucsbece154b_victim_cache.sv
+++ ucsbece154b_victim_cache.sv
@@ -169,7 +169,7 @@ function void lru_bump(input way_index_t way);
     // function to move way to MRU while maintaining DLL structure
     MEM_d[MEM_d[way].mru].lru = MEM_d[way].lru; // TODO
     MEM_d[MEM_d[way].lru].mru = MEM_d[way].mru; // TODO
-    lru_d = (way == lru_d) ? MEM_d[lru_d].mru : MEM_d[MEM_d[lru_d].mru].lru; // TODO
+    lru_d = (way == lru_d) ? MEM_d[lru_d].mru : lru_d; // TODO
     MEM_d[way].lru = mru_d; // TODO
     MEM_d[mru_d].mru =  way; // TODO
     mru_d = way; // TODO
